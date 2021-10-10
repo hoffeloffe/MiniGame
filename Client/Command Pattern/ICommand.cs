a0434906408c1be81c2b95099dae6733bf011026
TaiStudent <TaiStudent@student.eadania.dk>
2021-10-10 15:37:18 +0200

    Command Pattern

---
+++ Client/Command Pattern/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotAGame.Command_Pattern
+{
+    interface ICommand
+    {
+        public void Excute();
+    }
+}
---
+++ Client/Command Pattern/InputHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotAGame.Command_Pattern
+{
+    class InputHandler : ICommand
+    {
+        #region Singleton
+        private static InputHandler instance;
+        public InputHandler Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new InputHandler();
+                }
+                return instance;
+            }
+        }
+        #endregion
+        public Dictionary<Keys, ICommand> keybindings = new Dictionary<Keys, ICommand>();
+
+        public InputHandler()
+        {
+
+        }
+
+        public void Excute()
+        {
+            KeyboardState key = Keyboard.GetState();
+
+            foreach (Keys k in keybindings.Keys)
+            {
+
+            }
+        }
+    }
+}
